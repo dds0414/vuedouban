ceee866800429cd6bea537499e4b3f1fab500536
yangbo <yangbo@21eline.com>
2016-11-10 08:34:03 +0800

    add

---
+++ src/main-compiled.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Index from './components/index.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [{ path: '/', component: Index }]
+});
+new Vue({ router: router }).$mount('#app');
+
+//# sourceMappingURL=main-compiled.js.map
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import Index from './components/index.vue'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes : [
+        { path: '/', component: Index },
+    ]
+});
+new Vue({router:router}).$mount('#app');
