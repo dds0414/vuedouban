42bf5ac94bf0b785e6391c652e73b30243a29a56
yangbo <123202746@qq.com>
2016-11-14 00:25:47 +0800

    add page

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,39 +2,28 @@
 
     <div>
         <el-menu default-active="0" class="el-menu-demo" mode="horizontal">
-            <el-menu-item v-for="(item, index) in TagList" :index="String(index)">{{ item.tag_name }}</el-menu-item>
+            <el-menu-item v-for="(item, index) in TagList" :index="String(index)"><div @click="getBookListWithTagId(item.id)">{{ item.tag_name }}</div></el-menu-item>
         </el-menu>
-        <el-table
-                :data="BookList"
-                stripe
-                border
-                style="width: 100%">
-                <el-table-column
-                    prop="title"
-                    label="书名"
-                    width="280">
-            </el-table-column>
-            <el-table-column
-                    prop="star"
-                    label="评分"
-                    width="180">
-            </el-table-column>
-            <el-table-column
-                    prop="comment"
-                    label="评论人数">
-            </el-table-column>
-            <el-table-column
-                    prop="desc"
-                    label="书籍简介">
-            </el-table-column>
+        <el-table :data="BookList" stripe border @row-click="setDialog">
+            <el-table-column prop="title" label="书名" width="280"></el-table-column>
+            <el-table-column prop="star" label="评分" width="180"></el-table-column>
+            <el-table-column prop="comment" label="评论人数"></el-table-column>
+            <el-table-column prop="desc" label="书籍简介"></el-table-column>
         </el-table>
-        <!--<table style="clear:both;padding-top: 10px">-->
-            <!--<tr v-for="item in BookList">-->
-                <!--<td>{{ item.title }}</td>-->
-                <!--<td>评分：{{ item.star }}&nbsp;</td>-->
-                <!--<td>评论：({{ item.comment }})</td>-->
-            <!--</tr>-->
-        <!--</table>-->
+        <el-pagination
+                @current-change="handleCurrentChange"
+                :current-page="pageNum"
+                :page-size="10"
+                layout="total, prev, pager, next"
+                :total="100">
+        </el-pagination>
+        <el-dialog size="tiny" title="百度云PDF下载" v-model="dialogTableVisible">
+            <el-table :data="BookList" @row-click="testConsole">
+                <el-table-column  property="title" label="下载地址" width="510"></el-table-column>
+            </el-table>
+        </el-dialog>
+
+
 
     </div>
 </template>
@@ -69,40 +58,60 @@
 <script>
     var TagList = [];
     var BookList = [];
+    var pageNum = 2;
+    var initTagId = 1;
     export default{
 
         data: function () {
-            this.$http.get('http://localhost:8888/doubanserver/')
+            defaultId : 1
+            return {
+                TagList: TagList,
+                BookList: BookList,
+                initTagId : initTagId,
+                pageNum : pageNum,
+                dialogTableVisible: false
+            }
+        },
+        mounted:function(){
+            this.getTagList();
+            this.getBookListWithTagId(this.initTagId, 1);
+        },
+        methods: {
+            getTagList: function () {
+                this.$http.get('http://localhost:8888/doubanserver/')
                     .then(function (response) {
                         response.data.forEach(function (e) {
                             TagList.push(e);
                         })
                     });
-            return {
-                TagList: TagList,
-                BookList: BookList
-            }
-        },
-        mounted:function(){
-            this.getBookListWithTagId(1);
-        },
-        methods: {
-            getBookListWithTagId: function (id) {
-                this.$http.get('http://localhost:8888/doubanserver/index.php/Home/Index/getListWithTagId?id=' + id)
+            },
+            getBookListWithTagId: function (id, page) {
+                if(page){
+                    this.pageNum = page;
+                }else {
+                    this.pageNum = 1;
+                }
+                this.$http.get('http://localhost:8888/doubanserver/index.php/Home/Index/getListWithTagId?id=' + id +'&page=' + page)
                     .then(function (response) {
-                        BookList.splice(0,40);
-                        console.log(BookList);
+                        BookList.splice(0,10);
                         response.data.forEach(function (e) {
+                            if(e.title.length>50){
+                                e.title = e.title.substring(0,10) + "...";
+                            }
                             BookList.push(e);
                         });
                     });
+                this.defaultId = id;
             },
-            startHacking: function() {
-                this.$notify({
-                    title: 'It Works',
-                    message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
-                    duration: 6000
-                })
+            handleSizeChange(val) {},
+            handleCurrentChange(val) {
+                this.getBookListWithTagId(this.defaultId, val);
+            },
+            setDialog(row){
+                this.dialogTableVisible = true;
+            },
+            testConsole(row){
+                console.log(row.id);
             }
         }
     }
